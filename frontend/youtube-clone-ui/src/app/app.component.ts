546f1b7ebe91691802c7a0ecb84272998a1bb4c1
Nguyen Dinh Nhat <nhatnguyen188203@gmail.com>
2024-04-25 21:17:23 +0700

    add save-video-detail component

--- frontend/youtube-clone-ui/src/app/app.component.ts
+++ frontend/youtube-clone-ui/src/app/app.component.ts
@@ -7,6 +7,13 @@ import {HeaderComponent} from "./header/header.component";
 import {MatButtonModule} from "@angular/material/button";
 import {MatToolbarModule, MatToolbarRow} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {BrowserModule} from "@angular/platform-browser";
+import {VgCoreModule} from "@videogular/ngx-videogular/core";
+import {VgControlsModule} from "@videogular/ngx-videogular/controls";
+import {VgOverlayPlayModule} from "@videogular/ngx-videogular/overlay-play";
+import {VgBufferingModule} from "@videogular/ngx-videogular/buffering";
 
 
 @Component({
@@ -21,7 +28,13 @@ import {MatIconModule} from "@angular/material/icon";
     MatButtonModule,
     MatToolbarModule,
     MatIconModule,
-    MatToolbarRow
+    MatToolbarRow,
+    MatFormFieldModule,
+    FlexLayoutModule,
+    VgCoreModule,
+    VgControlsModule,
+    VgOverlayPlayModule,
+    VgBufferingModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- frontend/youtube-clone-ui/src/app/save-video-details/save-video-details.component.ts
+++ frontend/youtube-clone-ui/src/app/save-video-details/save-video-details.component.ts
@@ -1,7 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit, ViewChild} from '@angular/core';
 import {MatFormField, MatLabel} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
 import {MatOption, MatSelect} from "@angular/material/select";
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {COMMA, ENTER} from "@angular/cdk/keycodes";
+import {
+  MatChip, MatChipEditedEvent, MatChipGrid,
+  MatChipInput,
+  MatChipInputEvent, MatChipRow,
+} from "@angular/material/chips";
+import {MatIcon} from "@angular/material/icon";
+import {MatButton} from "@angular/material/button";
+import {LiveAnnouncer} from "@angular/cdk/a11y";
+
 
 @Component({
   selector: 'app-save-video-details',
@@ -11,14 +23,82 @@ import {MatOption, MatSelect} from "@angular/material/select";
     MatInput,
     MatSelect,
     MatOption,
-    MatLabel
+    MatLabel,
+    FlexLayoutModule,
+    ReactiveFormsModule,
+    MatChipInput,
+    MatChip,
+    MatIcon,
+    MatButton,
+    MatChipGrid,
+    MatChipRow
   ],
   templateUrl: './save-video-details.component.html',
   styleUrl: './save-video-details.component.css'
 })
-export class SaveVideoDetailsComponent {
-  constructor() { }
+export class SaveVideoDetailsComponent implements OnInit {
 
-  ngOnInit(): void {
+  saveVideoDetailsForm: FormGroup;
+  title: FormControl = new FormControl('');
+  description: FormControl = new FormControl('');
+  videoStatus: FormControl = new FormControl('');
+
+  constructor() {
+    this.saveVideoDetailsForm = new FormGroup({
+      title: this.title,
+      description: this.description,
+      videoStatus: this.videoStatus,
+    })
   }
+
+  ngOnInit(): void {}
+
+  addOnBlur = true;
+  readonly separatorKeysCodes = [ENTER, COMMA] as const;
+  tags: string[] = [];
+
+  announcer = inject(LiveAnnouncer);
+
+  add(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    // Add our fruit
+    if (value) {
+      this.tags.push(value);
+    }
+
+    // Clear the input value
+    event.chipInput!.clear();
+  }
+
+  remove(value: string): void {
+    const index = this.tags.indexOf(value);
+
+    if (index >= 0) {
+      this.tags.splice(index, 1);
+
+      this.announcer.announce(`Removed ${value}`);
+    }
+  }
+
+  edit(fruit: string, event: MatChipEditedEvent) {
+    const value = event.value.trim();
+
+    // Remove fruit if it no longer has a name
+    if (!value) {
+      this.remove(fruit);
+      return;
+    }
+
+    // Edit existing fruit
+    const index = this.tags.indexOf(fruit);
+    if (index >= 0) {
+      this.tags[index] = value;
+    }
+  }
+
+
+
 }
+
+
